862769160c7cd2b8a3448c1a7f74036ada42c507
mojiclub <mojiclub@protonmail.com>
2022-02-05 14:03:21 +0100

    Handle new contract

--- app.js
+++ app.js
@@ -1,41 +1,11 @@
-// Ethers.js
-var provider = '';
-var signer = '';
-var contract = '';
-const ABI = [
-  // Some details about the token
-  "function name() view returns (string)",
-  "function symbol() view returns (string)",
-
-  // Get the account balance
-  "function balanceOf(address) view returns (uint)",
-
-  // Send some of your tokens to someone else
-  "function transfer(address to, uint amount)",
-
-  // Getters
-  "function totalSupply() view returns (uint)",
-  "function PRICE_ETH_() view returns (uint256)",
-  "function MAX_MINT_() view returns (uint256)",
-  "function GEN0_SUPPLY_() view returns (uint256)",
-
-  // Ticket NFT claiming
-  "function claimTickets()",
-
-  // Mint token
-  "function mint(uint numberOfTokens) payable",
-
-  // Only for owner
-  "function withdraw()",
-
-  // An event triggered whenever anyone transfers to someone else
-  "event Transfer(address indexed from, address indexed to, uint amount)"
-];
-
 // Store last TX information
 var tx_id = '';
 var tx_pending = false;
 var last_tx_recept = '';
+
+if(!window.ethereum) {
+    document.getElementById("web3_status").style.display= 'none';
+}
 
 // Get the wallet balance in ETH
 async function signer_balance_eth(){
@@ -45,24 +15,57 @@ async function signer_balance_eth(){
     return parseFloat(balance);
 }
 
+/* Blockchain getters */
 async function mint_price_bc(){
-    var num = await contract.PRICE_ETH_();
+    var num = await contract.PRICE_ETH();
     return parseFloat(ethers.utils.formatEther(num));
 }
 
-async function max_mint_bc(){
-    var num = await contract.MAX_MINT_();
-    return num.toNumber();
-}
-
 async function gen0_supply_bc(){
-    var num = await contract.GEN0_SUPPLY_();
+    var num = await contract.GEN0_Max_Id();
     return num.toNumber();
 }
 
 async function nb_minted_bc(){
     var num = await contract.totalSupply();
     return num.toNumber();
+}
+
+async function max_mint_bc(){
+    var num_maxmint = await contract.MAX_MINT();
+    var num_minted = await nb_minted_bc();
+    var num_gen0supply = await gen0_supply_bc();
+    num_maxmint = num_maxmint.toNumber();
+
+    // If gen0 is still active, we need to make sure to return the max between left to mint and MAX_MINT.
+    if(num_minted < num_gen0supply){
+        return Math.min(num_gen0supply - num_minted, num_maxmint);
+    } else {
+        return num_maxmint;
+    }    
+}
+
+async function wl_date_bc(){
+    var ts = await contract.WL_MINT_TIMESTAMP();
+    var wl_date = new Date(ts.toNumber() * 1000);
+    return wl_date.toLocaleString('en-US', date_format_options);
+}
+
+async function mint_date_bc(){
+    var ts = await contract.MINT_TIMESTAMP();
+    var wl_date = new Date(ts.toNumber() * 1000);
+    return wl_date.toLocaleString('en-US', date_format_options);
+}
+
+async function reveal_date_bc(){
+    var ts = await contract.REVEAL_TIMESTAMP();
+    var wl_date = new Date(ts.toNumber() * 1000);
+    return wl_date.toLocaleString('en-US', date_format_options);
+}
+
+async function SaleIsActive(){
+    var b = await contract.SaleIsActive();
+    return !b;
 }
 
 function sleep(ms) {
@@ -109,25 +112,27 @@ async function mint(){
 $(document).ready(async function() {
 
     async function connect_wallet() {
-        // if(!window.ethereum){} handle this
         provider = new ethers.providers.Web3Provider(window.ethereum, "any");
-        await sleep(20);
-        if(provider._network.chainId!=CHAIN_ID){
+        // Weird enough, _network stays undefined for a few ms. We'll wait..
+        while(!provider._network){
+            await sleep(10);
+        }
+        if(provider._network.chainId != CHAIN_ID){
             await ethereum.request({
                 method: 'wallet_switchEthereumChain',
                 params: [{ chainId: '0x'+CHAIN_ID.toString(16) }],
               });
             provider = new ethers.providers.Web3Provider(window.ethereum, "any");
-            await sleep(20);
+            // Weird enough, _network stays undefined for a few ms. We'll wait..
+            while(!provider._network){
+                await sleep(10);
+            }
         }
         signer = provider.getSigner();
         var addr = await signer.getAddress();
         $('#eth_status').text(addr);
         contract = new ethers.Contract(CONTRACT_ADDRESS, ABI, provider);
-        var nb_tks = parseInt($("#nb_mint").val());
-        var mint_txt = "MINT "+nb_tks+" TOKEN";
-        if(nb_tks>1){mint_txt+="S";}
-        $('#Mint').text(mint_txt);
+        $("#nb_mint").trigger('mouseup');
         addr_a = addr.substring(0,7);
         addr_b = addr.substring(addr.length-7,addr.length);
         $("#web3_status").text(addr_a+"..."+addr_b);
@@ -144,26 +149,37 @@ $(document).ready(async function() {
         var blocknum = await provider.getBlockNumber();
         var txs = await contract.queryFilter(filtrr, blocknum-10000, blocknum);
         var addedHashs = [];
-        $("#web3_actions").html('<h2>Transactions in last 10.000 blocks</h2>');
-        for(const tx of txs.reverse()){
-            var hash = tx.transactionHash;
-            if(addedHashs.includes(hash)){
-                continue;
-            } else {
-                addedHashs.push(hash);
+        if(txs.length<0){
+            $("#web3_actions").html('<h2>Transactions (10.000 last blocks) :</h2>');
+            for(const tx of txs.reverse()){
+                var hash = tx.transactionHash;
+                if(addedHashs.includes(hash)){
+                    continue;
+                } else {
+                    addedHashs.push(hash);
+                }
+                var tx_link = '<p id="link_'+hash+'"><span class="tx_status">✅</span> : <a target="_blank" href="'+RPC_SCAN_URL+'/tx/'+hash+'">'+hash.substring(0,12)+'..'+hash.substring(hash.length-4,hash.length)+'</a></p>';
+                $("#web3_actions").append(tx_link);
             }
-            var tx_link = '<p id="link_'+hash+'"><span class="tx_status">✅</span> : <a target="_blank" href="'+RPC_SCAN_URL+'/tx/'+hash+'">'+hash.substring(0,12)+'..'+hash.substring(hash.length-4,hash.length)+'</a></p>';
-            $("#web3_actions").append(tx_link);
+        } else {
+            $("#web3_actions").html('<h2>No transactions (10.000 last blocks)</h2>');
         }
+        
     }
 
     $("#nb_mint").bind('keyup mouseup',async function () {
+        if(!SALE_ACTIVE) {
+            $('#mint_form').addClass("disabled");
+            $('#Mint').text("SALE DISABLED TEMPORARILY");
+            return;
+        }
+
         var nbtm = parseInt($("#nb_mint").val());
         if(nbtm>MAX_MINT){
             $("#nb_mint").val(MAX_MINT);
         }
-        if(signer!='' && !soldout){
-            mint_txt = "MINT "+nbtm+" TOKEN";
+        if(signer!=''){
+            mint_txt = "MINT "+nbtm+" GEN"+gen_number+" TOKEN";
             if(nbtm>1){mint_txt+="S";}
             $('#Mint').text(mint_txt); 
         }
@@ -200,12 +216,25 @@ $(document).ready(async function() {
         }
     });
 
-    // Check if metamask is installed
-    if(window.ethereum && window.ethereum.isMetaMask) {
-        await connect_wallet();
-    } else {
-        provider = new ethers.providers.JsonRpcProvider(RPC);
+    /* USER INTERFACE */
+
+    // Images on mobile
+    var rotate_imgs = 0;
+    $('.top_images.mobonly img').attr('src',proj_top_images[rotate_imgs]);
+    function rotate_imgs_f() {
+        rotate_imgs++;
+        $('.top_images.mobonly img').attr('src',proj_top_images[rotate_imgs%proj_top_images.length]);
     }
+
+    // Accent colors
+    var linear_gradient = "linear-gradient(217deg, "+proj_color+", "+proj_color2+")";
+    $("#Mint, #web3_status").css("background",linear_gradient);
+    $("#web3_actions").css("background",proj_color+"aa");
+    $('#mint_dates').css("color",proj_color);
+
+    setInterval(rotate_imgs_f, 1000);
+
+    provider = new ethers.providers.JsonRpcProvider(RPC);
     contract = new ethers.Contract(CONTRACT_ADDRESS, ABI, provider);
 
     // Pull informations from contract
@@ -213,45 +242,32 @@ $(document).ready(async function() {
     const MAX_MINT = await max_mint_bc();
     const GEN0_SUPPLY = await gen0_supply_bc();
     var NB_MINTED = await nb_minted_bc();
-    var soldout = NB_MINTED >= GEN0_SUPPLY;
-
-    var rotate_imgs = 0;
-    $('.top_images.mobonly img').attr('src',proj_top_images[rotate_imgs]);
-    //img_elem.src = proj_top_images[rotate_imgs];
-    function rotate_imgs_f() {
-        rotate_imgs++;
-        //img_elem.src = proj_top_images[rotate_imgs];
-        $('.top_images.mobonly img').attr('src',proj_top_images[rotate_imgs%proj_top_images.length]);
-    }
-
-    setInterval(rotate_imgs_f, 1000);
-
-
+    var gen0_soldout = NB_MINTED >= GEN0_SUPPLY;
+    var gen_number = gen0_soldout ? 1 : 0;
+    var SALE_ACTIVE = await SaleIsActive();
+    $("#nb_mint").trigger('mouseup');
     $("#nb_mint").attr("max",MAX_MINT);
 
-    // Add project elements
-    for(const elem of proj_top_images){
-        $('.top_images.deskonly').append('<img src="'+elem+'">');
-    }
-
+    // Project name and description
     $('#project_name').text(proj_name);
-    $('#project_description').text(proj_description);
-    $('#wl_mint').text(proj_wl_mint);
-    $('#public_mint').text(proj_public_mint);
-    //$('#p_nb_minted').text(proj_name);
+    $('#project_description').html(proj_description);
+
+    // Red box
+    d_proj_wl_mint = await wl_date_bc();
+    d_proj_public_mint = await mint_date_bc();
+    d_proj_reveal_date = await reveal_date_bc();
+    $('#wl_mint').text(proj_wl_mint + d_proj_wl_mint);
+    $('#public_mint').text(proj_public_mint + d_proj_public_mint);
+    $('#reveal_date').text(proj_reveal_date + d_proj_reveal_date);
+    $('#mint_dates').css("border","1px solid "+proj_color);
+
     $('#p_mint_price').text(MINT_PRICE + " Ξ");
     $('#span_total_supply').text(GEN0_SUPPLY);
-    // Project color
-    $('#mint_dates').css("border","1px solid "+proj_color);
-    var linear_gradient = "linear-gradient(217deg, "+proj_color+", "+proj_color2+")";
-    $("#Mint, #web3_status").css("background",linear_gradient);
-    $("#web3_actions").css("background",proj_color+"aa");
-    $('#mint_dates').css("color",proj_color);
 
-    // Check if collection is sold out
-    if(soldout){
+    // Check if gen0 is sold out
+    if(gen0_soldout){
         // Disable button
-        // Write soldout on it
+        // Write gen0_soldout on it
     }
     $('#span_nb_minted').text(NB_MINTED);
 
--- project_constants.js
+++ project_constants.js
@@ -9,30 +9,62 @@ const RPC_SCAN_URL = "https://ropsten.etherscan.io";
 // const RPC = "https://mainnet.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161" // ETH Mainnet
 // const CHAIN_ID = 1; // ETH Mainnet
 // const RPC_SCAN_URL = "https://etherscan.io";
+const CONTRACT_ADDRESS = '0x24ed45fbd53346f9f4c8215f92dd7b694ad86914';
 
+// Ethers.js
+var provider = '';
+var signer = '';
+var contract = '';
+const ABI = [
+  // Some details about the token
+  "function name() view returns (string)",
+  "function symbol() view returns (string)",
 
-const CONTRACT_ADDRESS = '0x8aaabf1925222fe79e5905233e0a84a3e8beee69';
-const MINT_PRICE = 0.08;
-const MAX_MINT = 5;
-const MAX_SUPPLY = 10000;
+  // Get the account balance
+  "function balanceOf(address) view returns (uint)",
 
-// NFT logo and images on top
-var proj_logo = 'https://anatomyscienceapeclub.com/_next/static/images/logo-banner-lg-m-700334b6cd4709f31aa8621eef0d13b0.png';
-var proj_top_images = [
-    'https://anatomyscienceapeclub.com/_next/static/images/m1-frosted-5-d04ffc4c7aff5d32221a26dc24b065b1.jpg',
-    'https://anatomyscienceapeclub.com/_next/static/images/m2-white-3-eec6462b14fa0396206d166586485bb2.jpg',
-    'https://anatomyscienceapeclub.com/_next/static/images/m2-gold-1-5bb8ffe9155a5f697ead540621850c0c.jpg',
-    'https://anatomyscienceapeclub.com/_next/static/images/m1-glow-3-91761278e95e18a3e4166ffe1203c44e.jpg'
+  // Send some of your tokens to someone else
+  "function transfer(address to, uint amount)",
+
+  // Getters
+  "function totalSupply() view returns (uint)",
+  "function PRICE_ETH() public view returns (uint256)",
+  "function MAX_MINT() public view returns (uint256)",
+  "function GEN0_Max_Id() public view returns (uint256)",
+  "function WL_MINT_TIMESTAMP() public view returns (uint256)",
+  "function MINT_TIMESTAMP() public view returns (uint256)",
+  "function REVEAL_TIMESTAMP() public view returns (uint256)",
+
+  // Ticket NFT claiming
+  "function claimTickets() public",
+
+  // Sale
+  "function SaleIsActive() public view returns (bool)",
+  "function mint(uint numberOfTokens) payable",
+
+  // Only for owner, remove later
+  "function withdraw()",
+
+  // An event triggered whenever anyone transfers to someone else
+  "event Transfer(address indexed from, address indexed to, uint amount)"
 ];
+
+// NFT images on top.
+var proj_top_images = [];
+var imgList = document.querySelectorAll('.top_images.deskonly img');
+for(elem of imgList) {proj_top_images.push(elem.src);}
 
 // Project name and description
 var proj_name = "Moji Club";
-var proj_description = "is a collection of 10,000 unique personnalized humans with unique traits. Mint yours to make it look like you !";
+var proj_description = "is a collection of unique personnalized humans with traits unique to you!. Mint yours to make it look like you !";
 
 // Listing date and details
-var proj_wl_mint = "Mint for WL users : 1 Feb ~ 2PM UTC";
-var proj_public_mint = "Public mint : 1 Feb ~ 3PM UTC";
+var proj_wl_mint = "Mint for WL users : ";
+var proj_public_mint = "Public mint : ";
+var proj_reveal_date = "Reveal : ";
+var date_format_options = {month: 'short', day: 'numeric', hour:'2-digit',minute:'2-digit', timeZone: 'UTC', timeZoneName:'short'};
 
 // Website accent colors
 var proj_color = "#DD5A56";
 var proj_color2 = "#DD5A56";
+
